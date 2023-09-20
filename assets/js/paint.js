c064cfc0e04a154544884f51cdeeec414c5e2489
tamccullough88 <tamccullough88@gmail.com>
2023-09-19 21:10:02 -0400

    added function to 'dim' the color swatches, formatted the html

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,27 +1,36 @@
 function configureListeners() {
-    let images = // select img elements  
+    let images =  document.getElementsByTagName("img")// select img elements  
 
 
      for (var i = 0; i < images.length; i++) {        
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)
+        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)
         // iterate over images and add mouseover event listeners      
     } 
 }
 
 function addOpacity(event) {
+    if (!this.classList.contains('dim')){
+        this.classList.add('dim')
+    }
     // add appropriate CSS class
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
+    if (this.classList.contains('dim')){
+        this.classList.remove('dim')
+    }
+
      //remove appropriate CSS class
 
-    let element = document.getElementById('color-price');
-        element.textContent = '';
+    // let element = document.getElementById('dataPrice');
+    //     element.textContent = '';
         
-    let color = document.getElementById('color-name');
-        color.textContent = ''; 
+    // let color = document.getElementByAlt('color-price');
+    //     color.textContent = ''; 
 
-    event.preventDefault();    
+    // event.preventDefault();    
 }
 
 function getProductInfo(paintColor) {
@@ -29,7 +38,7 @@ function getProductInfo(paintColor) {
     let colorName;  
     
     switch (paintColor) {
-        case 'pn1':           
+        case 'pn1':    
             // set variables for price and color name and invoke a function to update the price     
             break;           
         case 'pn2':
@@ -61,10 +70,10 @@ function getProductInfo(paintColor) {
 
     function updatePrice(colorName, price)
     {       
-        let colorPrice = // select element with corresponding id
+        let colorPrice = document.querySelector("dataPrice") // select element with corresponding id
         // display price
         
-        let color = // select element with corresponding id
+        let color = document.quesrSelector("alt")// select element with corresponding id
         //display color name
     }
     
